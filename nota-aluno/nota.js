350b442305d4867a02fa3680a6605e77e3f84c76
josué <josueematos15@gmail.com>
2024-10-19 10:48:50 -0300

    Sistema de nota de aluno
    Observaçao: criei o sistema mais pra testar um metodo de perguntas ao usuario de forma mais simples
    
    Simplicidade: Não há necessidade de lidar com callbacks, eventos ou Promises.
    Uso direto: O fluxo é linear, parecido com o que você vê no navegador com o prompt()

--- nota-aluno/nota.js
+++ nota-aluno/nota.js
@@ -1,16 +1,17 @@
-const readline = require('readline')
-
-const rl = readline.createInterface({
-
-    input: process.stdin,
-    output: process.stdout
-
-})
-
-rl.question("qual seu nome ?" , (resposta) =>{
-console.log (`ola  ${resposta} tudo bem?`)
+const prompt = require('prompt-sync')();
 
 
-    rl.close();
-});
+// Fazendo uma pergunta ao usuário
+var media = prompt('nota do primeiro bimestre ');
+var media2 = prompt('nota do segundo bimestre ');
+var media3 = prompt('nota do terceiro bimestre ');
+var media4 = prompt('nota do quarto bimestre ');
 
+    media =  parseFloat (media);
+    media2 = parseFloat (media2);
+    media3 = parseFloat (media3);
+    media4 = parseFloat (media4);
+
+    const resultado = (media + media2 + media3 + media4)/4;
+
+console.log(` resultado do aluno no final do bimestre ${ resultado} !`);
