21355f265f9eb5fe244e99d0539dc86c572cc552
josué <josueematos15@gmail.com>
2024-10-18 23:53:06 -0300

    Pergunta ao cliente

--- nota-aluno/nota.js
+++ nota-aluno/nota.js
@@ -14,19 +14,3 @@ console.log (`ola  ${resposta} tudo bem?`)
     rl.close();
 });
 
-// const readline = require('readline');
-
-// // Criando a interface readline para entrada e saída do terminal
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// // Fazendo uma pergunta ao usuário
-// rl.question("Qual é o seu nome? ", (resposta) => {
-//   console.log(`Olá, ${resposta}! Prazer em conhecê-lo.`);
-  
-//   // Fechando a interface após a resposta
-//   rl.close();
-// });
-
